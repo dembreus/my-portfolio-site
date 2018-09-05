2e895150c148aa3a582d561ce7c1828c02ad2cbb
Dillon <dillon.embreus@gmail.com>
2018-09-05 15:28:07 -0600

    added resume page
    updated skills, education, and expoerience

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,78 @@
-import React, { Component, Fragment } from 'react'
+import React, {Component, Fragment} from 'react'
+import {Grid, Cell} from 'react-mdl'
+import Education from './Education'
+import Experience from './Experience'
+import Skills from "./Skills";
 
-export default class Resume extends Component{
+export default class Resume extends Component {
     render() {
-        return(
-            <Fragment>
-                <h1>Resume page</h1>
-            </Fragment>
+        return (
+            <div>
+                <Grid>
+                    <Cell col={4}>
+                        <div style={{textAlign: 'center'}}>
+                            <img src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+                                 alt="avatar" style={{height: '200px'}}/>
+                        </div>
+                        <h2 style={{paddingTop: '2em'}}>Dillon Embreus</h2>
+                        <h4 style={{color: 'grey'}}>Programmer</h4>
+                        <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+                        <p>lorem ipsum</p>
+                        <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+                        <h5>Address</h5>
+                        <p>Salt Lake City, UT, 84124</p>
+                        <h5>Phone</h5>
+                        <p>(385) 444-6272</p>
+                        <h5>Email</h5>
+                        <p>dillon.embreus@gmail.com</p>
+                        <h5>Web</h5>
+                        <p>dillon-embreus.netlify.com</p>
+                        <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+                    </Cell>
+                    <Cell col={8}
+                          className="resume-right-col">
+                        <h2>Education</h2>
+                        <Education
+                            startYear={2014}
+                            endYear={'current'}
+                            schoolName={'Salt Lake Community College'}
+                            schoolDescription={"lorem ipsum"}
+                        />
+                        <Education
+                            startYear={2018}
+                            endYear={'current'}
+                            schoolName={'Helio Training Coding Bootcamp'}
+                            schoolDescription={"lorem ipsum"}
+                        />
+                        <hr style={{borderTop: '3px solid #e22947'}}/>
+                        <h2>Experience</h2>
+
+                        <Experience
+                            startYear={2018}
+                            endYear={'current'}
+                            jobName={'Piper Down Pub'}
+                            jobDescription={"lorem ipsum"}
+                        />
+                        <Experience
+                            startYear={2014}
+                            endYear={2017}
+                            jobName={'Tsunami Sushi Bar and Gril'}
+                            jobDescription={"lorem ipsum"}
+                        />
+                        <hr style={{borderTop: '3px solid #e22947'}}/>
+                        <h2>Skills</h2>
+                        <Skills
+                            skill={'JavaScript'}
+                            progress={55}/>
+                        <Skills
+                            skill={'React'}
+                            progress={35}/>
+                        <Skills
+                            skill={'Java'}
+                            progress={60}/>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
