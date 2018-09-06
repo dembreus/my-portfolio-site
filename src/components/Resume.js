26c04a7b91011c26557d6aa8034be53451889133
Dillon <dillon.embreus@gmail.com>
2018-09-05 18:51:26 -0600

    updated resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -17,9 +17,9 @@ export default class Resume extends Component {
                         </div>
                         <h2 style={{paddingTop: '2em'}}>Dillon Embreus</h2>
                         <h4 style={{color: 'grey'}}>Programmer</h4>
-                        <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+                        <hr style={{borderTop: '3px solid #2f94a3', width: '50%'}}/>
                         <p>lorem ipsum</p>
-                        <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+                        <hr style={{borderTop: '3px solid #2f94a3', width: '50%'}}/>
                         <h5>Address</h5>
                         <p>Salt Lake City, UT, 84124</p>
                         <h5>Phone</h5>
@@ -28,49 +28,114 @@ export default class Resume extends Component {
                         <p>dillon.embreus@gmail.com</p>
                         <h5>Web</h5>
                         <p>dillon-embreus.netlify.com</p>
-                        <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+                        <hr style={{borderTop: '3px solid #2f94a3', width: '50%'}}/>
                     </Cell>
                     <Cell col={8}
                           className="resume-right-col">
                         <h2>Education</h2>
                         <Education
-                            startYear={2014}
-                            endYear={'current'}
-                            schoolName={'Salt Lake Community College'}
-                            schoolDescription={"lorem ipsum"}
+                            startYear={'August 2014'}
+                            endYear={'Present'}
+                            schoolName={'Salt Lake City, UT, Salt Lake Community College, Computer Engineering'}
+                            schoolDescription={"Fundamentals of Computer Programming,\n" +
+                            "Object Oriented Programming,\n" +
+                            "Intro to C++,\n" +
+                            "Algorithms and Data Structures,"}
                         />
                         <Education
-                            startYear={2018}
-                            endYear={'current'}
-                            schoolName={'Helio Training Coding Bootcamp'}
-                            schoolDescription={"lorem ipsum"}
+                            startYear={'May 2018'}
+                            endYear={'August 2018'}
+                            schoolName={' Salt Lake City, UT, Helio Training Coding Bootcamp, Full Stack Web Development '}
+                            schoolDescription={"Certification of Course Completion,\n" +
+                            "Certification of Skills Proficiency"}
                         />
-                        <hr style={{borderTop: '3px solid #e22947'}}/>
+                        <hr style={{borderTop: '3px solid #34e89e'}}/>
                         <h2>Experience</h2>
 
                         <Experience
-                            startYear={2018}
-                            endYear={'current'}
-                            jobName={'Piper Down Pub'}
-                            jobDescription={"lorem ipsum"}
+                            startYear={'May 2017'}
+                            endYear={'Present'}
+                            jobName={'Bartender, Morris Pub Holdings Inc'}
+                            jobDescription={"Bartend a high-volume bar. Providing quality drinks, ensuring customer\n" +
+                            "satisfaction, and a high customer return rate. Deescalate disruptive\n" +
+                            "customers and disputes between staff members. Frequently surpass daily\n" +
+                            "sales goals. Account for the liquor and sales reports for all the employees\n" +
+                            "at the end of the night and make sure that the numbers stay on track.\n" +
+                            "Keep the bar safe from lawsuits by convincing intoxicated customers to\n" +
+                            "download taxi apps to avoid customers drinking and driving. "}
                         />
                         <Experience
-                            startYear={2014}
-                            endYear={2017}
-                            jobName={'Tsunami Sushi Bar and Gril'}
-                            jobDescription={"lorem ipsum"}
+                            startYear={'July 2016'}
+                            endYear={'May 2018'}
+                            jobName={'Bar-Back, Morris Pub Holdings Inc.'}
+                            jobDescription={"Responsibilities ranged from cleaning, running food and drinks, to taking\n" +
+                            "orders from customers and making drinks. Was the most preferred Bar\n" +
+                            "Back to work with, because my ability to anticipate the needs of the\n" +
+                            "bartender to ensure the bar operated swiftly."}
                         />
-                        <hr style={{borderTop: '3px solid #e22947'}}/>
+                        <hr style={{borderTop: '3px solid #34e89e'}}/>
                         <h2>Skills</h2>
                         <Skills
                             skill={'JavaScript'}
                             progress={55}/>
                         <Skills
+                            skill={'Java'}
+                            progress={60}/>
+                        <Skills
+                            skill={'C++'}
+                            progress={30}/>
+                        <Skills
+                            skill={'HTML'}
+                            progress={70}/>
+                        <Skills
+                            skill={'CSS'}
+                            progress={30}/>
+                        <Skills
                             skill={'React'}
                             progress={35}/>
                         <Skills
-                            skill={'Java'}
+                            skill={'Bootstrap'}
+                            progress={20}/>
+                        <Skills
+                            skill={'Skeleton'}
+                            progress={15}/>
+                        <Skills
+                            skill={'React MDL'}
+                            progress={35}/>
+                        <Skills
+                            skill={'Micro'}
+                            progress={20}/>
+                        <Skills
+                            skill={'Webstorm'}
                             progress={60}/>
+                        <Skills
+                            skill={'Visual Studio'}
+                            progress={35}/>
+                        <Skills
+                            skill={'GitHub'}
+                            progress={70}/>
+                        <Skills
+                            skill={'GitLab'}
+                            progress={25}/>
+                        <Skills
+                            skill={'GitKraken'}
+                            progress={60}/>
+                        <Skills
+                            skill={'MongoDB'}
+                            progress={20}/>
+                        <Skills
+                            skill={'Node.js'}
+                            progress={35}/>
+                        <Skills
+                            skill={'GIT'}
+                            progress={35}/>
+                        <Skills
+                            skill={'Prisma'}
+                            progress={20}/>
+                        <Skills
+                            skill={'GraphQL'}
+                            progress={35}/>
+
                     </Cell>
                 </Grid>
             </div>
